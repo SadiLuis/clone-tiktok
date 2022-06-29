6092d223c8928481ed537e1ce5c62a1fbec41f7f
SadiLuis <sadiluisrueda.94@gmail.com>
2022-06-29 16:37:59 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App () {
+ 
+
+  return (
+    <div className='App'>
+      <main>
+      Tiktok clone
+      </main>
+    </div>
+  )
+}
+
+export default App
