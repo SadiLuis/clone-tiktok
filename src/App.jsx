99d654ba20e7535cf715a334482a71943bf68573
SadiLuis <sadiluisrueda.94@gmail.com>
2022-07-08 16:51:45 -0300

    Actualizacion de repo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
 import './App.css'
-import VideoPlayer from './components/VideosPlayers'
+import { lazy, Suspense } from 'react'
+import { Route } from 'wouter'
+
+const Home = lazy(() => import('./pages/Home/index.jsx'))
+const Upload = lazy(() => import('./pages/Upload/index.jsx'))
+const Login = lazy(() => import('./pages/Login/index.jsx'))
 
 function App () {
   return (
     <div className='App'>
       <main>
-        <VideoPlayer />
+        <Suspense fallback={<div />}>
+          <Route path='/'>
+            <Home />
+          </Route>
+
+          <Route path='/upload'>
+            <Upload />
+          </Route>
+
+          <Route path='/login'>
+            <Login />
+          </Route>
+        </Suspense>
       </main>
     </div>
   )
--- src/components/VideosPlayers/index.jsx
+++ src/components/VideosPlayers/index.jsx
@@ -1,35 +1,34 @@
-import { useRef, useState } from 'react'
-import styles from './styles.module.css'
 import clsx from 'clsx'
+import { useRef } from 'react'
+import styles from './styles.module.css'
+import VideoPlayerActions from './VideoPlayerActions.jsx'
+import VideoDescription from '../VideoDescription/index.jsx'
+import useIntersectionVideoPlayer from '../../hooks/useIntersectionVideoPlayer.js'
 
-const SRC_URL = 'https://v16-webapp.tiktok.com/8b96325dd4b7e53600fcac7769e02962/62be2e10/video/tos/useast2a/tos-useast2a-pve-0068/0e2b8632231941a599de3439e2d85346/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=3272&bt=1636&btag=80000&cs=0&ds=3&ft=eXd.6HmzMyq8Z-_Z7we2NEleyl7Gb&mime_type=video_mp4&qs=0&rc=ODQ8NjhpOWllO2hpPGc1OEBpM2tpbGc6ZnNnZDMzNzczM0AzMi5jYV8wNmMxMjE2LV9fYSNxbWRzcjRfcHBgLS1kMTZzcw%3D%3D&l=20220630171244010189066023222A53EE'
-
-export default function VideoPlayer () {
-  const [isPlaying, setIsPlaying] = useState(false)
+export default function VideoPlayer (props) {
   const video = useRef(null)
+  const { playing, handlePlay } = useIntersectionVideoPlayer({ video })
 
-  const handlePlay = () => {
-    isPlaying
-      ? video.current.pause()
-      : video.current.play()
-
-    setIsPlaying(!isPlaying)
-  }
-
-  const playerClassname = clsx(styles.player, {
-    [styles.hidden]: !isPlaying
+  const playerClassName = clsx(styles.player, {
+    [styles.hidden]: playing
   })
 
+  const { src } = props
+
   return (
-    <div>
+    <div className={styles.wrapper}>
       <video
         className={styles.video}
-        src={SRC_URL}
         controls={false}
-        ref={video}
+        loop
         onClick={handlePlay}
+        ref={video}
+        src={src}
       />
-      <i className={playerClassname} onClick={handlePlay} />
+      <i className={playerClassName} onClick={handlePlay} />
+      <VideoPlayerActions {...props} />
+      <VideoDescription {...props} />
     </div>
+
   )
 }
